f5dee47fc43cb8965c0773c6ea7343339b7f5f08
mraphael2101ntt <mark.raphael@emeal.nttdata.com>
2024-10-16 12:13:04 +0100

    WIP

---
+++ src/test/java/org/example/TestGetPostsAnotherExample.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class TestGetPostsAnotherExample {
+
+    @Test
+    public void testGetPosts() {
+
+        String base_url = "https://jsonplaceholder.typicode.com";
+        String endpoint = base_url + "/posts";
+
+        Response response = given().
+                when().
+                get(endpoint);
+
+        assertThat(response.statusCode(), is(200));
+
+        //response.then().log().all();
+
+        List<String> titles = response.jsonPath().getList("title");
+
+        for (String title : titles) {
+            System.out.println(title);
+            assertThat(title, notNullValue());
+         }
+
+        assertThat(titles.get(1), equalTo("sunt aut facere repellat provident occaecati excepturi optio reprehenderit"));
+
+    }
+
+}
