34581d86abe82ae117ccd66770a6534ef442438f
mraphael2101ntt <mark.raphael@emeal.nttdata.com>
2024-10-18 15:18:43 +0100

    WIP

--- src/test/java/org/example/TestGetPosts_Sellamall.java
+++ src/test/java/org/example/TestGetPosts_Sellamall.java
@@ -11,7 +11,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
-public class TestGetPostsAnotherExample {
+public class TestGetPosts_Sellamall {
 
     @Test
     public void testGetPosts() {
